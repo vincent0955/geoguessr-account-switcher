a9a0b4850ce13665b3be20047a9deb7405305bfe
vincent0955 <74017998+vincent0955@users.noreply.github.com>
2021-08-07 13:54:40 -0400

    popup.js

--- popup.js
+++ popup.js
@@ -5,5 +5,10 @@
 //   }
 
 function switcher(user, pass){
-  return https://www.geoguessr.com/signout?target=%2F;
+  window.open("https://www.geoguessr.com/signout?target=%2F");
+  window.open("https://www.geoguessr.com/signin");
+  document.getElementsByName(username).value = user;
+  document.getElementsByName(password).value = pass;
+  document.getElementsByClassName("button button--medium button--primary")[0].click();
+  return 0;
 }
