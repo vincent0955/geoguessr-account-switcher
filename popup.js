7644a577366d89a7cc5e09d02fa3a0f5b8a67879
vincent0955 <74017998+vincent0955@users.noreply.github.com>
2021-08-07 13:39:36 -0400

    add files

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
-var ele = document.getElementsByClassName('chat-line__message-container tw-relative');
+// var ele = document.getElementsByClassName('chat-line__message-container tw-relative');
 
-for (var i = 0; i < ele.length; i++) {
+// for (var i = 0; i < ele.length; i++) {
     
-  }
-  
+//   }
+
+function switcher(user, pass){
+  return https://www.geoguessr.com/signout?target=%2F;
+}
